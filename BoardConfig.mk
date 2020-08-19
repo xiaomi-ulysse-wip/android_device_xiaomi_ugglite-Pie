05814203f4b5da6b1bf7c49b6911086344676869
zqm64 <zqm64@outlook.com>
2020-08-19 17:29:19 +0800

    ugg: Use ulysse kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -124,7 +124,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := ugg_defconfig
-TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
+TARGET_KERNEL_SOURCE := kernel/xiaomi/ulysse
 TARGET_COMPILE_WITH_MSM_KERNEL	:= true
 
 # Lights
